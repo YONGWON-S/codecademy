1a7b00cbaa080eaa53088047f88e553ee7480b8e
Yongwon Seo <yongwon.seo.dev@gmail.com>
2019-01-31 23:56:17 +1100

    React!

---
+++ Codecademy/React/React_basic.js
@@ -0,0 +1,7 @@
+const myTeam = {
+  center: <li>Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGuard: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billon</li>
+}; // react library에선 javascript내에서 html 요소들이 쓰일 수 있다. 대신 compilie 과정이 필요하다
